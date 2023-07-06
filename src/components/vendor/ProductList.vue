e651240e3fee0f7cd96bfa4259ed90ee355aa45c
program39 <jerielformento@gmail.com>
2023-07-06 11:04:28 +0800

    Vendor Product List (update)
    Removed unused element
    Removed console logs

--- src/components/vendor/ProductList.vue
+++ src/components/vendor/ProductList.vue
@@ -50,7 +50,7 @@
                         New Product
                     </router-link>
                     <div class="flex items-center space-x-3 w-full md:w-auto">
-                        <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-sm border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
+                        <!-- <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-sm border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
                             <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                 <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
                             </svg>
@@ -65,7 +65,7 @@
                             <div class="py-1">
                                 <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Delete all</a>
                             </div>
-                        </div>
+                        </div> -->
                         <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-sm border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
                             <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
@@ -333,8 +333,6 @@
                         let gb = [];
 
                         response.data.map(function(value, key) {
-
-                            console.log(value.id + "|" + sel_brand);
                             gb.push({
                                 id: value.id,
                                 description: value.description,
@@ -360,7 +358,7 @@
             });
             const products = await getMyProducts(1);
             const productItems = ref(products[0][0]);
-            console.log(products[1][0]);
+            
             return { 
                 NoImageUrl: import.meta.env.VITE_NO_IMAGE_URL,
                 pagination: products[1][0],
