ff557ac80e988b1f01f45bd75e08901ba019555f
program39 <jerielformento@gmail.com>
2023-10-05 00:59:02 +0800

    Fixed props issue - rev1
    Fixed issue with props

--- src/components/forms/InputText.vue
+++ src/components/forms/InputText.vue
@@ -1,8 +1,8 @@
 <template>
-    <label :for="this.$props.name" class="block text-sm font-medium leading-6">{{ this.$props.label }}</label>
-    <div class="relative" v-if="this.$props.type === 'password'">
-        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :class="{ 'ring-2 ring-inset ring-red-500': this.$props.error !== '' }"
-            :id="this.$props.name" :name="this.$props.name" :type="inputTypePassword" required
+    <label :for="props.name" class="block text-sm font-medium leading-6">{{ props.label }}</label>
+    <div class="relative" v-if="props.type === 'password'">
+        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :class="{ 'ring-2 ring-inset ring-red-500': props.error !== '' }"
+            :id="props.name" :name="props.name" :type="inputTypePassword" required
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6">
         <button @click.prevent="toggleTypePassword" type="button" tabindex="-1"
             class="absolute right-2.5 bottom-2.5 inline-flex flex-shrink-0 justify-center items-center w-5 h-5 ml-[-1px] px-3 py-1.5 text-sm font-semibold leading-6 text-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-950 rounded-r-sm">
@@ -10,24 +10,24 @@
             <EyeSlashIcon v-else class="absolute right-0 bottom-0 top-0" />
         </button>
     </div>
-    <div class="mt-2" v-else-if="this.$props.type === 'group_text'">
+    <div class="mt-2" v-else-if="props.type === 'group_text'">
         <div class="relative">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                <span class="text-sm text-gray-400 font-semibold">{{ this.$props.prefix }}</span>
+                <span class="text-sm text-gray-400 font-semibold">{{ props.prefix }}</span>
             </div>
-            <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :class="{ 'ring-2 ring-inset ring-red-500': this.$props.error !== '' }" required 
-            :id="this.$props.name" :name="this.$props.name" type="text" autocomplete="off"
+            <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :class="{ 'ring-2 ring-inset ring-red-500': props.error !== '' }" required 
+            :id="props.name" :name="props.name" type="text" autocomplete="off"
             class="block w-full rounded-md border-0 pl-12 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6">
         </div>
-        <small v-if="this.$props.error !== ''" class="text-red-400">{{ this.$props.error }}</small>
+        <small v-if="props.error !== ''" class="text-red-400">{{ props.error }}</small>
     </div>
     <div class="mt-2" v-else>
-        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :class="{ 'ring-2 ring-inset ring-red-500': this.$props.error !== '' }"
-            :required="!this.$props.optional"
-            :id="this.$props.name" :name="this.$props.name" :type="this.$props.type" autocomplete="off"
-            :placeholder="this.$props.placeholder"
+        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :class="{ 'ring-2 ring-inset ring-red-500': props.error !== '' }"
+            :required="!props.optional"
+            :id="props.name" :name="props.name" :type="props.type" autocomplete="off"
+            :placeholder="props.placeholder"
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:text-sm sm:leading-6">
-        <small v-if="this.$props.error !== ''" class="text-red-400">{{ this.$props.error }}</small>
+        <small v-if="props.error !== ''" class="text-red-400">{{ props.error }}</small>
     </div>
 </template>
 <script setup>
@@ -37,10 +37,11 @@ import { EyeIcon, EyeSlashIcon } from "@heroicons/vue/24/outline";
 <script>
 export default {
     props: ['label', 'error', 'name', 'type', 'placeholder', 'prefix', 'modelValue', 'optional'],
-    data() {
+    data(props) {
         const inputTypePassword = ref("password");
 
         return {
+            props,
             inputTypePassword
         }
     },
